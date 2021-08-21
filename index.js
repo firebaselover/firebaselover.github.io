93afecb0e3421446a01796b89ef29e55cfef2572
firebaselover <89289447+firebaselover@users.noreply.github.com>
2021-08-21 11:01:39 +0530

    corrected code

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ firebase.auth().onAuthStateChanged(function(user) {
 
       var email_id = user.email;
       document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-      //window.open("http://mywebsite.com/home.html");
 
     }
 
