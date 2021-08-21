f966a3901061e7d483f097361fa441fb5a475992
firebaselover <89289447+firebaselover@users.noreply.github.com>
2021-08-21 10:53:30 +0530

    added new welcome page after login

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ firebase.auth().onAuthStateChanged(function(user) {
     if(user != null){
 
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      //document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      window.open("http://mywebsite.com/home.html");
 
     }
 
